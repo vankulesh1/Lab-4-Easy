5189951db5e11d197240404ad3dfbfb4cb6f181f
NataliiaKulesh <ivankulesh0200@gmail.com>
2024-01-08 22:46:00 +0200

    enter word

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,9 +1,13 @@
 package org.example;
-
+import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
 
-        String exampleWord = "madam";
-        System.out.println("Is '" + exampleWord + "' a palindrome? " + PalindromeChecker.isPalindrome(exampleWord));
+        System.out.println("Enter a word to check if it's a palindrome: ");
+        String userWord = scanner.nextLine();
+
+
+        System.out.println("Is '" + userWord + "' a palindrome? " + PalindromeChecker.isPalindrome(userWord));
     }
 }
