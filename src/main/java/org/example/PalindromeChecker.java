33c9a648ec0b4222c4107744ec02ea0317356a9c
NataliiaKulesh <ivankulesh0200@gmail.com>
2024-01-09 21:29:18 +0200

    enter word

--- src/main/java/org/example/PalindromeChecker.java
+++ src/main/java/org/example/PalindromeChecker.java
@@ -1,15 +1,21 @@
 package org.example;
+
 public class PalindromeChecker {
 
     public static boolean isPalindrome(String word) {
-
-        String cleanedWord = word.replaceAll("\\s+", "").toLowerCase();
-
-
+        String cleanedWord = removeSpacesAndBigLetter(word);
         String reversedWord = new StringBuilder(cleanedWord).reverse().toString();
-
-
         return cleanedWord.equals(reversedWord);
     }
 
+    private static String removeSpacesAndBigLetter(String input) {
+        StringBuilder result = new StringBuilder();
+        for (char ch : input.toCharArray()) {
+            if (!Character.isWhitespace(ch)) {
+
+                result.append(Character.toLowerCase(ch));
+            }
+        }
+        return result.toString();
+    }
 }
